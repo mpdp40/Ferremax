6e414dfed9fab3f41f3741be4c2d25fa670e0a40
mpdp40 <127349678+mpdp40@users.noreply.github.com>
2025-06-04 16:07:43 -0400

    Update paypal_client.py

--- myapp/paypal_client.py
+++ myapp/paypal_client.py
@@ -1,9 +1,1 @@
-from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
 
-class PayPalClient:
-    def __init__(self):
-        self.client_id = 'AfjWw16xI3aGBAMWyf-kYWpUYLQpezAjH26UidNNlQhTFoGHHBocl8ZbVVJeo0eSdndUAmdEyXQ0iNJ7'
-        self.client_secret = 'EOI01wPO3L4By8_SdQ72aW0s9vLt9NXdTrHFZDuWEHbmxi3ZGiun_HQeL6r3BjGEzmM4yoBUhAwc4LR7'
-
-        self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
-        self.client = PayPalHttpClient(self.environment) 
